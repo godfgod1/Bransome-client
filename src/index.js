fc3e5d208f785c0b269d8d5f4bafb8c939b52605
hsh411 <pixeldot12@gmail.com>
2021-04-05 15:57:53 +0900

    Add CSS,Pages Folder && Modify action,type,store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import Bransome from "./Bransome";
 import store from "./store/index";
+import "./css/index.css";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Bransome />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
