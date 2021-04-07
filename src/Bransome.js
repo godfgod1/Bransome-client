fde3b9607b66d9934d33aa90b8f6e4522bfe9c5b
hsh411 <pixeldot12@gmail.com>
2021-04-07 10:47:09 +0900

    Add NavBar and Modify CSS Styling

--- src/Bransome.js
+++ src/Bransome.js
@@ -1,6 +1,6 @@
 import react from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Navigation from "./components/Navgation";
+import Navigation from "./components/Navigation";
 import SwitchRoute from "./Pages/Switch";
 function Bransome() {
   return (
---
+++ src/components/Navigation.js
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import "../css/navigation.css";
+import logo from "../logos/bransome3.png";
+const Navigation = () => {
+  return (
+    <nav id="navbar_box">
+      <div className="navbar_container">
+        <span className="navbar_logo">
+          <Link to="/">
+            <img src={logo} />
+          </Link>
+        </span>
+        <ul className="navbar_links">
+          <li className="navbar_item">
+            <Link to="/" className="navbar_link_btn navbar_home_btn">
+              Home
+            </Link>
+          </li>
+          <li className="navbar_item">
+            <Link to="/brand" className="navbar_link_btn navbar_brand_btn">
+              Brands
+            </Link>
+          </li>
+          <li className="navbar_item">
+            <Link to="/best20" className="navbar_link_btn navbar_best_btn">
+              Best20
+            </Link>
+          </li>
+
+          <li className="navbar_item">
+            <a
+              className="navbar_link_btn"
+              href="https://github.com/codestates/Bransome-client"
+            >
+              Blog
+            </a>
+          </li>
+          <li className="navbar_link_btn navbar_item">
+            <button className="navbar_sign_in_btn">로그인</button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
