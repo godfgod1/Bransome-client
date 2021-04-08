63c9216920161ab90b4eb3618b87652d045ed222
hsh411 <pixeldot12@gmail.com>
2021-04-08 17:02:00 +0900

    Add Carousel Beta1 && ADD Fake File

--- src/Pages/sections/StoryView.js
+++ src/Pages/sections/StoryView.js
@@ -1,7 +1,56 @@
+import React, { useState, useRef, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import slider from "./fakedata/fakeComments";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
+
+let length = slider.length - 1;
+console.log(
+  slider.map((s, i) => {
+    return s.title, s.urls;
+  }),
+);
 const StoryView = () => {
+  const dispatch = useDispatch();
+  const clickNextSlide = () => dispatch();
+  const clickPrevSlide = () => dispatch();
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide(currentSlide === length ? 0 : currentSlide + 1);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [currentSlide]);
+
+  const prevSlides = () => {
+    setCurrentSlide(currentSlide === 1 ? length : currentSlide - 1);
+  };
+  const nextSlides = () => {
+    setCurrentSlide(currentSlide === length ? 0 : currentSlide + 1);
+  };
+
   return (
     <article className="home_view_container2">
-      <div data-aos="zoom-in" className="view_2"></div>
+      <div className="view2_content_box">
+        <div>캐로셀박스</div>
+        <button onClick={prevSlides}>버튼</button>
+        <button onClick={nextSlides}>버튼</button>
+        {currentSlide}
+        <section className="carousel_screen_container">
+          {slider.map((slide, idx) => (
+            <div
+              key={idx}
+              className={
+                idx === currentSlide ? "slides active" : "slide-content"
+              }
+            >
+              <img className="slide-image" src={slide.urls} alt="" />
+              <h3 className="slide-title">{slide.title}</h3>
+            </div>
+          ))}
+        </section>
+      </div>
     </article>
   );
 };
--- src/redux-Moduls/Actions.js
+++ src/redux-Moduls/Actions.js
@@ -1,11 +1,35 @@
 /* type */
 
 export const ACTIONS = {};
-
+export const NEXT_SLIDE = "NEXT_SLIDE";
+export const PREV_SLIDE = "PREV_SLIDE";
 /* ACTIONS */
 
-const a = () => {};
+// const a = () => {};
 
-const b = () => {};
+// const b = () => {};
 
-export const actionCreators = { a, b };
+// export const actionCreators = { a, b };
+
+const nextSlide = () => {
+  return {
+    type: NEXT_SLIDE,
+    payloads: {
+      activeIdx: 0,
+      translate: 0,
+      transition: 0.45,
+    },
+  };
+};
+
+const prevSlide = () => {
+  return {
+    type: PREV_SLIDE,
+    payloads: {
+      activeIdx: 0,
+      translate: 0,
+      transition: 0.45,
+    },
+  };
+};
+export const actionCreators = { nextSlide, prevSlide };
