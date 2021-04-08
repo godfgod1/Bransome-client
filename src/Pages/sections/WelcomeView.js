a5ec366dd55756c6aa57315435bf7678b9c4eb9d
hsh411 <pixeldot12@gmail.com>
2021-04-08 11:31:17 +0900

    Modify Nav and Mainpage CSS Styling

--- src/Pages/sections/WelcomeView.js
+++ src/Pages/sections/WelcomeView.js
@@ -1,15 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import "../../css/welcomeview.css";
+
 const WelcomeView = () => {
   return (
     <article className="home_view_container1">
       <div className="view_1">
         <h1 data-aos="zoom-in">브랜드의 모든 것</h1>
         <h2 data-aos="zoom-in">브랜썸.</h2>
-        <h3 data-aos="zoom-in">쉽고 빠르게 영감을 얻어가세요.</h3>
+        <p data-aos="zoom-in">쉽고 빠르게 영감을 얻어가세요.</p>
         <div className="search" data-aos="fade-up">
-          <input type="search"></input>
+          <input
+            type="search"
+            placeholder="검색창에 입력하세요"
+            className="home_search"
+          />
           <FontAwesomeIcon icon={faSearch} className="search_icon" />
         </div>
       </div>
