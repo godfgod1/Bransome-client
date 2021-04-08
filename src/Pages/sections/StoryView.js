abe7ed4e6eba9478d83df345098f01ccafbee611
hsh411 <pixeldot12@gmail.com>
2021-04-08 10:40:24 +0900

    Modify Pages && Modify Nav CSS styling && ADD sections Pages

---
+++ src/Pages/sections/StoryView.js
@@ -0,0 +1,9 @@
+const StoryView = () => {
+  return (
+    <article className="home_view_container2">
+      <div data-aos="zoom-in" className="view_2"></div>
+    </article>
+  );
+};
+
+export default StoryView;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import Bransome from "./Bransome";
 import store from "./redux-Moduls/Store";
 import "./css/index.css";
+import "./css/navigation.css";
+import "./css/welcomeview.css";
+import "./css/commentview.css";
+import "./css/storyview.css";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
