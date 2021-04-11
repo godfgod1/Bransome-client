d3877250ef1f9094f904cba25f34017fd484c973
spark0109 <seonabak0109@gmail.com>
2021-04-11 14:32:43 +0900

    hold on

--- src/Pages/BrandsPage.js
+++ src/Pages/BrandsPage.js
@@ -9,10 +9,20 @@ import netflix from "../images/test.netflix.png";
 const Brand = () => {
   return (
     <div>
-      <h1>Brands</h1>
-      <Link to="/brandinfo" exact className="Brand-logo">
-        <img id="logo" src={netflix} />
-      </Link>
+      <div id= "left-side"><div>좋아하는 브랜드를 마이페이지에 담아보세요</div>
+      <div>
+        <input>
+        <img src="https://i.pinimg.com/474x/b6/ce/20/b6ce200fec0ebe98d1ccd69a7adb7796.jpg"/>
+        </input>
+        </div>
+        <div class= "tag"><span>전체</span><span>카페/프랜차이즈</span>
+        <span>전자기기</span><span>IT/미디어</span><span>자동차</span>
+        </div></div>
+        <div id="right-side">
+          <Link to="/brandinfo" exact className="Brand-logo">
+            <img id="logo" src={netflix} />
+          </Link>
+        </div>
     </div>
   );
 };
